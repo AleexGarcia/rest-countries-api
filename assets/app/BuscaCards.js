5a9aebf979146ab0740e3e115bc2d97063bd639d
Alexandre Garcia <alexandre_augusto_garcia@hotmail.com>
2022-08-26 13:15:32 -0300

    classes card, cardlist, function buscaCard

---
+++ assets/app/BuscaCards.js
@@ -0,0 +1,50 @@
+class Card {
+
+    constructor(flags, name, population, region, subregion, capital, domain, currencies, languages, borders) {
+        this.flags = flags;
+        this.name = name;
+        this.population = population;
+        this.region = region;
+        this.subregion = subregion;
+        this.capital = capital;
+        this.tld = domain;
+        this.currencies = currencies;
+        this.languages = languages;
+        this.borders = borders;
+
+    }
+
+}
+class ListCard {
+    constructor() {
+        this.cards = [];
+    }
+    adiciona(card) {
+        this.cards.push(card);
+    }
+}
+const ListaDeCards = new ListCard;
+
+async function buscaCard() {
+    try {
+        let consultaCountries = await fetch("https://restcountries.com/v3.1/all");
+        let consultaCountriesConvertida = await consultaCountries.json();
+        consultaCountriesConvertida.forEach(element => {
+            ListaDeCards.adiciona(new Card(
+                element.flags.svg,
+                element.name,
+                element.population,
+                element.region,
+                element.subregion,
+                element.capital,
+                element.tld,
+                element.currencies,
+                element.languages,
+                element.borders
+            ));
+        });
+    } catch (erro) {
+        console.log(erro);
+    }
+}
+
