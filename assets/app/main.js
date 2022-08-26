0048eb35dbee2c2f07c8a3e5b4dd9684826b7a8f
Alexandre Garcia <alexandre_augusto_garcia@hotmail.com>
2022-08-26 17:10:14 -0300

    unificando as funções em 1 js

--- assets/app/main.js
+++ assets/app/main.js
@@ -1,2 +1,74 @@
+const cartas = document.querySelector("#cartas");
+class Card {
+
+    constructor(flags, name, population, region, subregion, capital, domain, currencies, languages, borders) {
+        this.flags = flags;
+        this.name = name;
+        this.population = population;
+        this.region = region;
+        this.subregion = subregion;
+        this.capital = capital;
+        this.tld = domain;
+        this.currencies = currencies;
+        this.languages = languages;
+        this.borders = borders;
+
+    }
+
+}
+class ListCard {
+    constructor() {
+        this.cards = [];
+    }
+    adiciona(card) {
+        this.cards.push(card);
+    }
+}
+const ListaDeCards = new ListCard;
+
+async function buscaCard() {
+    try {
+        let consultaCountries = await fetch("https://restcountries.com/v3.1/all");
+        let consultaCountriesConvertida = await consultaCountries.json();
+        consultaCountriesConvertida.forEach(element => {
+            ListaDeCards.adiciona(new Card(
+                element.flags.png,
+                element.name,
+                element.population,
+                element.region,
+                element.subregion,
+                element.capital,
+                element.tld,
+                element.currencies,
+                element.languages,
+                element.borders
+            ));
+        });
+        criaCards();
+       
+    } catch (erro) {
+        console.log(erro);
+    }
+}
+
+function criaCards() {
+    ListaDeCards.cards.forEach((element, index) => {
+        index = document.createElement("div");
+        cartas.appendChild(index);
+        index.innerHTML = `
+        <figure class="card">
+            <img class="card__img" src="${element.flags}" alt="Bandeira: ${element.name.common}">
+            <figcaption class="card__caption">
+                <h2 class="card__title">${element.name.common}</h2>
+                <div class="card__information">
+                    <span class="card__population"><span class="card__population--span">Population: </span> ${element.population}</span>
+                    <span class="card__region"><span class="card__region--span">Region: </span> ${element.region}</span>
+                    <span class="card__capital"><span class="card__capital--span">Capital: </span>${element.capital}</span>
+                </div>
+            </figcaption>
+        </figure>
+        `
+    });
+}
 
 buscaCard();
